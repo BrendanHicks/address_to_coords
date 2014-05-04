a62c4e912ddb4c668f4915504fab37b280e3bcac
BrendanHicks <Brendan-Hicks@kellogg.northwestern.edu>
2014-05-04 14:17:25 -0500

    Added Error Handling

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,15 +3,36 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
+
+    if params[:location] == nil
+
+      @address = "Diversey and Southport"
+
+    else
+
+          @address = params[:location]
+
+    end
+
+
     @url_safe_address = URI.encode(@address)
             full_url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=true"
 
         raw_data = open(full_url).read
             parsed_data = JSON.parse(raw_data)
                 results = parsed_data["results"]
+
+                       if results[0] != nil
+
+
                         @longitude   = results[0]["geometry"]["location"]['lng']
                         @latitude    = results[0]["geometry"]["location"]["lat"]
+
+                       else
+
+                        @longitude = 0.0
+                        @latitude = 0.0
+                    end
 
   end
 end
